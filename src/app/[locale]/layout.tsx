3cb056db923d8cf6202f5d42b3323d5e210dca82
heojeonguk <hellsong90@gmail.com>
2026-04-25 21:36:23 +0900

    fix: 임시 페이지로 빌드 오류 수정

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,19 +1,8 @@
-import { locales } from '@/middleware'
-
-export async function generateStaticParams() {
-  return locales.map((locale) => ({ locale }))
-}
-
 export default function LocaleLayout({
   children,
-  params,
 }: {
   children: React.ReactNode
   params: { locale: string }
 }) {
-  return (
-    <div lang={params.locale}>
-      {children}
-    </div>
-  )
+  return <div>{children}</div>
 }
