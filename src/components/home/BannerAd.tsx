03e059e01d03e226b059cf5a759b4fec9366eb78
heojeonguk <hellsong90@gmail.com>
2026-05-03 22:04:55 +0900

    fix: 쿠팡파트너스 배너 Script 컴포넌트로 수정

--- src/components/home/BannerAd.tsx
+++ src/components/home/BannerAd.tsx
@@ -1,12 +1,23 @@
 'use client'
+import Script from 'next/script'
 
 export default function BannerAd() {
   return (
     <div className="w-full flex justify-center my-4">
-      <script src="https://ads-partners.coupang.com/g.js"></script>
-      <script dangerouslySetInnerHTML={{__html: `
-        new PartnersCoupang.G({"id":985823,"template":"carousel","trackingCode":"AF8718289","width":"680","height":"140","tsource":""});
-      `}} />
+      <div id="coupang-banner" />
+      <Script
+        src="https://ads-partners.coupang.com/g.js"
+        onLoad={() => {
+          new (window as any).PartnersCoupang.G({
+            id: 985823,
+            template: 'carousel',
+            trackingCode: 'AF8718289',
+            width: '680',
+            height: '140',
+            tsource: ''
+          })
+        }}
+      />
     </div>
   )
 }
